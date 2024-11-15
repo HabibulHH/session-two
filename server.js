fadb3fbbe6515b4fdbdc56a1edc8dd03b8f10407
HabibulHH <habibul.hasan.hira@gmail.com>
2024-11-15 16:38:49 +0600

    added node js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const hostname = '127.0.0.1';
+const hostname = '209.97.171.155';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -17,6 +17,7 @@ const server = http.createServer((req, res) => {
       }
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
       res.end(data);
     });
   } else {
@@ -28,4 +29,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+});
